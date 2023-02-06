113deff0ef51f1718e8b6346fc238e91ec7e61ad
sergd <videomc1979@gmail.com>
2023-02-06 16:23:47 +0300

    add HW_24

---
+++ views.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, jsonify
+from marshmallow import ValidationError
+
+from builder import build_query
+from models import RequestSchema, BatchRequestSchema
+
+main_bp = Blueprint('main', __name__)
+
+# FILE_NAME = 'data/apache_logs.txt'
+
+
+@main_bp.route("/perform_query", methods=['POST'])
+def perform_query():
+    data = request.json
+
+    try:
+        validate_data = BatchRequestSchema().load(data)
+    except ValidationError as error:
+        return jsonify(error.messages), 400
+
+    result = None
+    for query in validate_data['queries']:
+        result = build_query(
+            cmd=query['cmd'],
+            value=query['value'],
+            # file_name=query['file_name'],
+            file_name=validate_data['file_name'],
+            data=result,
+        )
+
+    return jsonify(result)
