92ca130edbd21d9dfe9740b5061681fa436a9e22
sergd <videomc1979@gmail.com>
2023-02-11 09:11:40 +0300

    add HW_24 regex_query matebook

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from typing import Any
 
 from views import main_bp
 
-def create_app():
+def create_app() -> Any:
     app = Flask(__name__)
     app.register_blueprint(main_bp)
     return app
