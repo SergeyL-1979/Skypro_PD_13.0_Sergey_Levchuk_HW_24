5724c0827c23ab653b787b64cb94a67e27d42afd
sergd <videomc1979@gmail.com>
2023-02-11 13:13:48 +0300

    add HW_24 typing func

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from typing import Any
+from typing import Optional, Any
 
 from views import main_bp
 
-def create_app() -> Any:
+def create_app() -> Optional[Any]:
     app = Flask(__name__)
     app.register_blueprint(main_bp)
     return app
